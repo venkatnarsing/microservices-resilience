5ce67875f38d66d1282b322012abbec2e3a44069
venkatnarsing <41180069+venkatnarsing@users.noreply.github.com>
2020-02-27 12:25:23 +0530

    Add files via upload

---
+++ movie-catalog-microservice/src/main/java/com/example/microserviceresilience/MicroserviceResilienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceresilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceResilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceResilienceApplication.class, args);
+	}
+
+}
