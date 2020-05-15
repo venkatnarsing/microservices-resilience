b10c0154048b75bcf2be09702888baf79a15b433
Venkateshwarlu Narsing <venarsin@VENARSIN-45R1W.partnet.cisco.com>
2020-05-15 12:22:56 +0530

    Modified.

--- demo/src/main/java/com/example/demo/DemoResource.java
+++ demo/src/main/java/com/example/demo/DemoResource.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/demo")
+@RestController
 public class DemoResource {
 
-    @GetMapping
+    @RequestMapping("/hello/{name}")
     public String sayHello(@PathVariable("name") String name){
         return "Hello " + name;
     }
